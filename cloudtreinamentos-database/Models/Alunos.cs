0e7711aba741ddcbb538e2133af160a3815fdd92
augusto araujo <augroberto@outlook.com>
2022-04-13 12:48:03 -0300

    Complete solution

---
+++ cloudtreinamentos-database/Models/Alunos.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace cloudtreinamentos.Models
+{
+    public class Alunos
+    {
+        public int id { get; set; } 
+        public string name { get; set; }
+    }
+}
---
+++ cloudtreinamentos-database/Models/Blobs.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace cloudtreinamentos.Models
+{
+    public class Blobs
+    {
+        public string URL { get; set; }
+    }
+}
